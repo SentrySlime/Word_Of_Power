24a698ffdd15fa985c517e90dc2ec5f2b2641671
SentrySlime <sebastian.osterberg@hotmail.se>
2020-12-23 10:54:47 +0100

    Small Changes

---
+++ Assets/Scripts/Enemy_System/EnemyBehavior/EnemyMotor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyMotor : MonoBehaviour
+{
+
+    NavMeshAgent enemyAgent;
+    Camera cam;
+
+    void Start()
+    {
+        enemyAgent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
+    }
+
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                EnemyMove(hit.point);
+            }
+
+        }
+    }
+
+    public void EnemyMove(Vector3 destination)
+    {
+        enemyAgent.SetDestination(destination);
+    }
+
+
+}
